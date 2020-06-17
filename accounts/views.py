cbf454c71242935d75eac49524ad83bd922d36b7
AhnDongwon <lemontech119@gmail.com>
2020-06-17 16:10:28 +0900

    Fix 마이페이지 image error 수정

--- accounts/views.py
+++ accounts/views.py
@@ -5,24 +5,13 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import UserCreateSerializer, UserLoginSerializer, ProfilePageSerializer
 from .models import User, Profile
-from config.settings.production import MEDIA_URL
+from teams.models import Team
 
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def createUser(request):  # 회원가입 ?
-    '''
-    (req)
-    email
-    username
-    password
-    passwordCheck -- pass
-    (res)
-    message
 
-    :param request:
-    :return:
-    '''
     if request.method == 'POST':
         # password_check = request.data.pop('passwordCheck')  #
         serializer = UserCreateSerializer(data=request.data)
@@ -42,18 +31,7 @@ def createUser(request):  # 회원가입 ?
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def login(request):
-    '''
-    (req)
-    email
-    password
-    (res)
-    success
-    username
-    token
 
-    :param request:
-    :return:
-    '''
     if request.method == 'POST':
         serializer = UserLoginSerializer(data=request.data)
 
@@ -102,55 +80,28 @@ class ProfileView(RetrieveUpdateAPIView):
     # queryset 속성이 필요한가 ? list 도 아닌데 ?
 
     def retrieve(self, request, *args, **kwargs):
-        '''
-        (req)
-        header - token
-        (res)
-        username
-        livingArea
-        phone
-        email
-        image !!!
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        '''
 
         # print('뷰 함수')
         user = request.user
         profile = Profile.objects.get(user=user)
 
-        # image eb에 올라가서 open() 할때 에러가 나는 듯
-        # image = MEDIA_URL + str(profile.image.open())
-
         response = {
             'success': 'True',
-            'username': user.username,
-            'livingArea': profile.livingArea,
-            'phone': profile.phone,
-            'email': user.email,
-            'image': "image url"
+            'profile': {
+                'username': user.username,
+                'livingArea': profile.livingArea,
+                'phone': profile.phone,
+                'email': user.email,
+                'image': profile.image.url
+            }
+
         }
         return Response(response, status=status.HTTP_200_OK)
 
     def update(self, request, *args, **kwargs):
-        '''
-        (req)
-        username
-        livingArea
-        phone
-        email
-        (res)
-        message : OK, Fail(409)
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        '''
+
         # user = request.user  # User.objects.filter(email=request.data['email']).first()
         serializer = self.get_serializer(instance=request.user, data=request.data)
-        # User 로 참조하기
 
         if not serializer.is_valid(raise_exception=True):
             return Response({"message": "Request Body Error."}, status=status.HTTP_409_CONFLICT)
