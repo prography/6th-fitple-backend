a6f2f807a72537d5e1f03a7cb12feab38331a899
AhnDongwon <lemontech119@gmail.com>
2020-07-03 18:12:55 +0900

    feat/mypage application 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('user/check/', views.userCheck),
     path('user/profile/<int:pk>/', views.getProfile),
     path('profile/', views.ProfileView.as_view()),
+    path('profile/application/', views.application),
     path('test/', views.test) # celery email test
 ]
