79ac4918ceffdab3f00a97b71e2a2ca72afddef5
AhnDongwon <lemontech119@gmail.com>
2020-07-31 00:14:27 +0900

    Remove 가입환영 메잉

--- accounts/views.py
+++ accounts/views.py
@@ -34,7 +34,8 @@ def createUser(request):  # 회원가입 ?
             user = serializer.save()  # 추가로 멤버 주입할 수 있었던듯
             # existing instance 제공하면 update 실행된다
 
-            def_welcome_email.delay(user.email)  # 가입 인사 메일
+            # 임시로 막아놓겠습니다.
+            # def_welcome_email.delay(user.email)  # 가입 인사 메일
 
             return Response({"message": "ok"}, status=status.HTTP_201_CREATED)
         return Response({"message": "duplicate email"}, status=status.HTTP_409_CONFLICT)
